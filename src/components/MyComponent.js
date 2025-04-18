d37863cd6508df993eb09dbf39f9b7ee73dcf8d1
CDQuan88 <duyquan7b@gmail.com>
2025-04-18 22:36:32 +0700

    React Component

---
+++ src/components/MyComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>My first component {Math.random()}</div>
+        )
+    }
+}
+
+export default MyComponent;
